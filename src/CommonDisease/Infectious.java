3d8bbb922ff90672f763746ad421951bfb8770be
Dr. Arif <drarifsun@gmail.com>
2017-12-13 22:50:23 -0500

    A new folder created with infectious disease

---
+++ src/CommonDisease/Infectious.java
@@ -0,0 +1,13 @@
+package CommonDisease;
+
+public class Infectious {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tuberculosis is a common infectious disease");
+		System.out.println("Caused by Mycobacterium Tuberculi");
+		System.out.println("Symptoms are cough with weight loss");
+		System.out.println("And low grade fever");
+	}
+
+}
