96ba46e1115d985671e8e4f6a565a966af391067
Dr. Arif <drarifsun@gmail.com>
2017-12-13 23:06:52 -0500

    Vaccine name added

--- src/CommonDisease/Infectious.java
+++ src/CommonDisease/Infectious.java
@@ -1,3 +1,8 @@
+/*  Practicing Java
+     Article by Dr. Arif
+     Infectious chronic diseases
+*/
+
 package CommonDisease;
 
 public class Infectious {
@@ -8,6 +13,8 @@ public class Infectious {
 		System.out.println("Caused by Mycobacterium Tuberculi");
 		System.out.println("Symptoms are cough with weight loss");
 		System.out.println("And low grade fever");
+		System.out.println("To prevent TB, right after birth");
+		System.out.print("BCG vaccine is given");
 	}
 
 }
