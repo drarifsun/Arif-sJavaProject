b56d268bd90071934f470ba7db948a335df1cc1c
Dr. Arif <drarifsun@gmail.com>
2017-12-15 19:54:28 -0500

    small change in Animal>Parasites

--- src/Animals/Parasites.java
+++ src/Animals/Parasites.java
@@ -10,7 +10,7 @@ public class Parasites {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Parasites live on a host body");
+		System.out.println("Parasites live and nourish on a host body");
 		System.out.println("They get food from host");
 		System.out.println("Some are symbiotic, others harmful");
 	}
