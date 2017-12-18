f035217672c48bfd8a0f7aa0f868991b98b67354
drarifsun <34174617+drarifsun@users.noreply.github.com>
2017-12-18 15:50:12 -0500

    Small changes of organs affected.

--- src/CommonDisease/Infectious.java
+++ src/CommonDisease/Infectious.java
@@ -9,9 +9,10 @@ public class Infectious {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tuberculosis is a common infectious disease");
-		System.out.println("Tuberculosis often called TB");
-                System.out.println("It ia usually attacks the lungs");
+		System.out.println("Tuberculosis is a common infectious disease \nWhich primarily involves lungs and bones less frequently");
+		// merged line 13  iwth line 12
+		
+		System.out.println("It is often called TB");
                 System.out.println("But it can attack almost any part of the body");
                 System.out.println("TB can be an active disease or latent infection");
 		System.out.println("Caused by Mycobacterium Tuberculi");
